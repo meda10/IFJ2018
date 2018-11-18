545343fbdc536f25eb663be5bf97b2c5f1d42f9c
meda10 <meda735@gmail.com>
2018-11-18 22:14:02 +0100

    Tabulka symbolu

--- generator.c
+++ generator.c
@@ -11,18 +11,40 @@
 #include "string.h"
 #include "symtable.h"
 
+/*
+ * .IFJcode18
+JUMP $$main
+LABEL $$main
+ */
+
+int currentLabel;
 
 void startGenerating() {
+
     inScope = true;
 
     printf(".IFJcode18\n");
 
-    //todo generovani vestavenych funkci
+  
     printf("JUMP $$main\n");
     printf("LABEL $$main\n");
+
 }
 
-char *getVarFrame() {
+
+char *getNewLabel() {
+    char *newLabel = (char *) malloc(30 * sizeof(char));
+    sprintf(newLabel, "%%WL%d", ++currentLabel);
+    return newLabel;
+}
+
+char *getHelpRegister() {
+    char *reg = (char *) malloc(30 * sizeof(char));
+    sprintf(reg, "%%HR%d", currentHelpRegister++);
+    return reg;
+}
+
+char *get_frame() {
     char *frameTP = (char *) malloc(30 * sizeof(char));
     if (!inScope) {
         strcpy(frameTP, "LF");
@@ -31,6 +53,25 @@ char *getVarFrame() {
     }
     return frameTP;
 }
+
+char *get_type(int type) {
+    char *t = (char *) malloc(10 * sizeof(char));
+    switch (type) {
+        case INTEGER_TYPE:
+            strcpy(t, "int");
+            break;
+        case DOUBLE_TYPE:
+            strcpy(t, "float");
+            break;
+        case STRING_TYPE:
+            strcpy(t, "string");
+            break;
+        default:
+            break;
+    }
+    return t;
+}
+
 
 void generate_if(){
 
@@ -49,7 +90,7 @@ void generate_print(int type, string *value) {
             printf("WRITE string@%s\n", value->str);
             break;
         case IDENTIFIER:
-            printf("WRITE %s@%s\n", getVarFrame(), value->str);
+            printf("WRITE %s@%s\n", get_frame(), value->str);
             break;
         default:
             break;
@@ -59,16 +100,50 @@ void generate_print(int type, string *value) {
 
 void variable_declare(int type, string *name, bool used) {
     if (used) {
-        printf("DEFVAR %s@%s\n", getVarFrame(), name->str);
+        printf("DEFVAR %s@%s\n", get_frame(), name->str);
+        double a = 0;
         switch (type) {
             case INTEGER_TYPE:
-                printf("MOVE %s@%s int@0\n", getVarFrame(), name->str);
+                printf("MOVE %s@%s int@0\n", get_frame(), name->str);
                 break;
             case DOUBLE_TYPE:
-                printf("MOVE %s@%s float@0\n", getVarFrame(), name->str);
+                printf("MOVE %s@%s float@%a\n", get_frame(), name->str, a);
                 break;
             case STRING_TYPE:
-                printf("MOVE %s@%s string@\n", getVarFrame(), name->str);
+                printf("MOVE %s@%s string@\n", get_frame(), name->str);
+                break;
+        }
+    }
+}
+
+
+
+void variable_assign(int expresion_type, BTNode var1, BTNode var2) {
+    if (1) { // todo bool
+        char *t = get_type(var1->data.type);
+        switch (expresion_type) {
+            case INT_E: {
+                printf("MOVE %s@%s %s@%d\n", get_frame(), var1->data.name->str, t, string_To_Int(var2->data.name)); //todo %d
+                break;
+            }
+            case DOUBLE_E: {
+                printf("MOVE %s@%s %s@%a\n", get_frame(), var1->data.name->str, t, string_to_Double(var2->data.name));
+                break;
+            }
+            case STRING_E:
+                printf("MOVE %s@%s %s@%s\n", get_frame(), var1->data.name->str, t, var2->data.name->str);
+                break;
+            case VARIABLE_E:
+                if ((var1->data.type == INTEGER_TYPE && var2->data.type == DOUBLE_TYPE) || (var1->data.type == DOUBLE_TYPE && var2->data.type == INTEGER_TYPE)) {
+
+                    //todo konvert
+
+                } else {
+                    printf("MOVE %s@%s %s@%s\n", get_frame(), var1->data.name->str, get_frame(),var2->data.name->str);
+                }
+                break;
+
+            default:
                 break;
         }
     }
